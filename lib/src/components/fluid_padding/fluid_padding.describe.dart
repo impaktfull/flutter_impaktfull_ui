66db63ee649e86a83c5a8db1d826c1e531a361ce
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-10-07 10:36:26 +0200

    fix: describe fluidpadding

--- lib/src/components/fluid_padding/fluid_padding.describe.dart
+++ lib/src/components/fluid_padding/fluid_padding.describe.dart
@@ -3,6 +3,7 @@ part of 'fluid_padding.dart';
 String _describeInstance(
     BuildContext context, ImpaktfullUiFluidPadding instance) {
   final descriptor = ComponentDescriptor();
+  descriptor.add('breakPoints', instance.breakPoints);
   descriptor.add('theme', instance.theme);
   return descriptor.describe();
 }
