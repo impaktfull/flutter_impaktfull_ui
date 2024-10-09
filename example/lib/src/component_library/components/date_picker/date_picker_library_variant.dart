b1d3e4ce5c48307b7ba334af07153165cfe05708
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-10-09 14:53:18 +0200

    feat: added DatePicker.range to the example

--- example/lib/src/component_library/components/date_picker/date_picker_library_variant.dart
+++ example/lib/src/component_library/components/date_picker/date_picker_library_variant.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:impaktfull_ui_2/impaktfull_ui.dart';
 import 'package:impaktfull_ui_example/src/component_library/components/date_picker/date_picker_library_item.dart';
 import 'package:impaktfull_ui_example/src/component_library/config/component_library_item.dart';
+import 'package:impaktfull_ui_example/src/widget/components/components_library_variant_descriptor.dart';
 
 class DatePickerLibraryVariant
     extends ComponentLibraryVariant<DatePickerLibraryPrimaryInputs> {
@@ -21,29 +22,29 @@ class DatePickerLibraryVariant
           inputs.selectedEndDate.updateState(null);
         },
       ),
-      // ComponentsLibraryVariantDescriptor(
-      //   wrapWithCard: true,
-      //   padding: const EdgeInsets.symmetric(vertical: 16),
-      //   child: ImpaktfullUiDatePicker(
-      //     selectedDate: inputs.selectedStartDate.value,
-      //     onDateChanged: (value) {
-      //       inputs.selectedStartDate.updateState(value);
-      //       inputs.selectedEndDate.updateState(null);
-      //     },
-      //     margin: const EdgeInsets.symmetric(horizontal: 16),
-      //   ),
-      // ),
-      // ComponentsLibraryVariantDescriptor(
-      //   wrapWithCard: true,
-      //   padding: const EdgeInsets.symmetric(vertical: 16),
-      //   child: ImpaktfullUiDatePicker.range(
-      //     selectedStartDate: inputs.selectedStartDate.value,
-      //     selectedEndDate: inputs.selectedEndDate.value,
-      //     onStartDateChanged: inputs.selectedStartDate.updateState,
-      //     onEndDateChanged: inputs.selectedEndDate.updateState,
-      //     margin: const EdgeInsets.symmetric(horizontal: 16),
-      //   ),
-      // ),
+      ComponentsLibraryVariantDescriptor(
+        wrapWithCard: true,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ImpaktfullUiDatePicker(
+          selectedDate: inputs.selectedStartDate.value,
+          onDateChanged: (value) {
+            inputs.selectedStartDate.updateState(value);
+            inputs.selectedEndDate.updateState(null);
+          },
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+        ),
+      ),
+      ComponentsLibraryVariantDescriptor(
+        wrapWithCard: true,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ImpaktfullUiDatePicker.range(
+          selectedStartDate: inputs.selectedStartDate.value,
+          selectedEndDate: inputs.selectedEndDate.value,
+          onStartDateChanged: inputs.selectedStartDate.updateState,
+          onEndDateChanged: inputs.selectedEndDate.updateState,
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+        ),
+      ),
     ];
   }
 
