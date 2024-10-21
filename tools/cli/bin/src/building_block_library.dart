89355b52548e9300a3bb95c8977dece27a6b3831
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-10-21 09:48:04 +0200

    fix: generator

--- tools/cli/bin/src/building_block_library.dart
+++ tools/cli/bin/src/building_block_library.dart
@@ -54,24 +54,22 @@ Future<void> _createBuildingBlockLibrary({
 }) async {
   final snakeCaseComponentName = componentName.pascalToSnakeCase();
   // Create dir
-  final path = join('example', 'lib', 'src', 'building_block_library',
-      'building_blocks', snakeCaseComponentName);
+  final path = join('example', 'lib', 'src', 'building_block_library', 'building_blocks', snakeCaseComponentName);
   final dir = Directory(path);
   if (dir.existsSync()) {
-    throw Exception('Component already exists');
+    throw Exception('Building Block already exists');
   }
   dir.createSync(recursive: true);
 
   // Create files
-  final libraryItemFile =
-      File(join(dir.path, '${snakeCaseComponentName}_library_item.dart'));
+  final libraryItemFile = File(join(dir.path, '${snakeCaseComponentName}_building_block.dart'));
 
   final libaryItemFileContent = """import 'package:flutter/widgets.dart';
 import 'package:impaktfull_ui_2/impaktfull_ui.dart';
 import 'package:impaktfull_ui_example/src/building_block_library/config/building_block_library_item.dart';
 
-class ${componentName}LibraryItem extends BuildingBlockLibraryItem {
-  const ${componentName}LibraryItem();
+class ${componentName}BuildingBlock extends BuildingBlockLibraryItem {
+  const ${componentName}BuildingBlock();
 
   @override
   String get title => '$componentClassName';
