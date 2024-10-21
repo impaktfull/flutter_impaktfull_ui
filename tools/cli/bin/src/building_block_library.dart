c33c89135da978e6e73d92f1066d995a06d9c6ce
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-10-21 09:41:56 +0200

    fix: import

--- tools/cli/bin/src/building_block_library.dart
+++ tools/cli/bin/src/building_block_library.dart
@@ -68,7 +68,7 @@ Future<void> _createBuildingBlockLibrary({
 
   final libaryItemFileContent =
       """import 'package:impaktfull_ui_2/impaktfull_ui.dart';
-package:impaktfull_ui_example/src/building_block_library/config/building_block_library_item.dart';
+import 'package:impaktfull_ui_example/src/building_block_library/config/building_block_library_item.dart';
 import 'package:impaktfull_ui_example/src/component_library/config/component_library_inputs.dart';
 
 class ${componentName}LibraryItem extends BuildingBlockLibraryItem {
